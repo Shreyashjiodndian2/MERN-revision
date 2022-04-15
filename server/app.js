5bc9c85be89996e83b836558369870f5ce488894
Shreyash kumar pandey <Shreyash.pandey.katni@gmail.com>
2022-04-15 14:59:22 +0530

    'server side done'

--- server/app.js
+++ server/app.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
-
+const schema = require('./schema/schema');
 const app = express();
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://127.0.0.1:27017/author');
+mongoose.connection.once('open', () => {
+    console.log('connected to database');
+});
+
+
+
 
 // bind express with graphql
 app.use('/graphql', graphqlHTTP({
-    // pass in a schema property
+    schema: schema,
+    graphiql: true
 }));
 
 app.listen(4000, () => {
